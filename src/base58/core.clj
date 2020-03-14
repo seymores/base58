dd66dd64fd7cec1bb364a27a0621bc3a1533aa9b
Ping <>
2020-03-14 23:17:07 +0800

    Add doc, add one more test

--- src/base58/core.clj
+++ src/base58/core.clj
@@ -1,4 +1,7 @@
-(ns base58.core)
+(ns base58.core
+  "Base58 is a binary-to-text encoding algorithm that’s designed to encode a 
+   blob of arbitrary binary data into human readable text, much like the more 
+   well known Base64 algorithm.")
 
 (def ^:const alphabet "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
 
@@ -10,6 +13,7 @@
     :else (bigint (.getBytes s))))
 
 (defn encode
+  "Encode s to base58 text value."
   [s]
   (loop [data (decode_unsigned s)
          hash ""]
--- test/base58/core_test.clj
+++ test/base58/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [base58.core :as b58]))
 
-(deftest encode
-  (testing "Enoding string to base58 value"
+(deftest encode-string
+  (testing "Encoding string to base58 value"
     (is (= (b58/encode "hello") "Cn8eVZg"))))
+
+(deftest encode-number
+  (testing "Encoding number to base58 value"
+    (is (= (b58/encode 12345) "4fr"))))
